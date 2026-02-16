5b9df156a55de35e0d519a5eceb228cb255f3e99
David James <david@createwithplay.com>
2026-02-16 11:34:59 +0000

    Initial Commit

---
+++ djay-Intro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  djay-Intro
+//
+//  Created by David James on 16/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ djay-IntroTests/djay_IntroTests.swift
@@ -0,0 +1,17 @@
+//
+//  djay_IntroTests.swift
+//  djay-IntroTests
+//
+//  Created by David James on 16/02/2026.
+//
+
+import Testing
+@testable import djay_Intro
+
+struct djay_IntroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
